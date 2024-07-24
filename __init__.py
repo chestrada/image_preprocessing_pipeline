02c9be724bd9bc9a073964cc85473dba55419ec0
Christian Estrada <chestrada@g.ucla.edu>
2024-07-24 11:45:17 -0700

    Update init file for version tracking

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
 from image_preprocessing_pipeline.process_images import merge_all_channels
+import subprocess
+import os
+
+def get_latest_tag():
+    try:
+        submodule_dir = os.path.dirname(__file__)
+        
+        latest_tag = subprocess.check_output(
+            ['git', 'describe', '--tags', '--abbrev=0'],
+            cwd=submodule_dir
+        ).strip().decode('utf-8')
+        
+        return latest_tag
+    except subprocess.CalledProcessError:
+        return 'unknown'
