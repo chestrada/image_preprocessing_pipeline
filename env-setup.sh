d8d829f53c7a7cbb4f964dee4bd95ba4c4cfeb4c
Christian Estrada <chestrada@g.ucla.edu>
2024-09-19 15:32:58 -0700

    Simple checks

--- env-setup.sh
+++ env-setup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# echo "Setting up conda environment..." 
+ENV_NAME="stitching"
 
-# conda env create -f environment.yml
+# Check if conda is installed
+if ! command -v conda &>/dev/null; then
+    echo "Anaconda (or Miniconda) is not installed. Please install or load it before running this script."
+    exit 1
+fi
 
-# echo "done." 
-
-# conda activate stitching
-
-# echo "Running pip install..." 
-
-# pip install requirements.txt
-
-# echo "done." 
+# Check if the environment already exists
+if conda info --envs | grep -q "$ENV_NAME"; then
+    echo "Activating existing Anaconda environment: $ENV_NAME"
+    conda activate "$ENV_NAME"
+else
+    echo "Setting up conda environment..." 
+    conda env create -f environment.yml -n "$ENV_NAME"
+    if_error_echo "Problem creating Anaconda environment"
+    conda activate "$ENV_NAME"
+    if_error_echo "Could not activate environment, this file must be sourced"
+    pip install -r requirements.txt
+    if_error_echo "Problem installing pip requirements"
+    echo "Environment ready"
+fi
 
 echo "Updating image_preprocesing_pipeline module" 
-
-# if [ -d "ZetaStitcher" ]; then
-#     echo "ZetaStitcher directory already exists. Pulling the latest changes."
-#     cd ZetaStitcher
-#     git pull origin master
-#     cd ..
-# else
-#     git clone https://github.com/LifeCanvas-Technologies/ZetaStitcher.git
-# fi
 
 git remote add upstream https://github.com/ucla-brain/image-preprocessing-pipeline.git
 
@@ -33,7 +33,4 @@ git checkout main
 
 git merge upstream/main
 
-
 echo "done." 
-
-
