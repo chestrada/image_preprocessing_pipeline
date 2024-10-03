9fd6de96673df776a13e995579e66b77e34df86c
Christian Estrada <chestrada@g.ucla.edu>
2024-10-03 16:03:54 -0700

    Accurate name import

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from image_preprocessing_pipeline.process_images import merge_all_channels
-from image_preprocessing_pipeline.convert import main
+from image_preprocessing_pipeline.convert import main as convert
 import subprocess
 import os
 
